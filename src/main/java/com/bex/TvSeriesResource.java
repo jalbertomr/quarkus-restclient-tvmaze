8366a6feaaf5f08be2ce2af674aaebace5b1e057
jalbertomr <jalbertomr@yahoo.com>
2023-04-27 01:25:29 -0500

    retrieve using simple restclient

---
+++ src/main/java/com/bex/TvSeriesResource.java
@@ -0,0 +1,32 @@
+package com.bex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import com.bex.model.TvSerie;
+import com.bex.proxy.TvSeriesProxy;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/tvseries")
+public class TvSeriesResource {
+	
+	@RestClient
+	TvSeriesProxy tvSeriesProxy;
+	
+	private List<TvSerie> tvSeries = new ArrayList<TvSerie>();
+    
+	@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get() {
+		TvSerie tvSerie = tvSeriesProxy.get("girls");
+		tvSeries.add(tvSerie);
+        return Response.ok(tvSeries).build();
+    }
+}
---
+++ src/main/java/com/bex/proxy/TvSeriesProxy.java
@@ -0,0 +1,27 @@
+package com.bex.proxy;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import com.bex.model.TvSerie;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+/**
+ * https://api.tvmaze.com/singlesearch/shows?q=game%20of%20thrones
+ * @author bext
+ *
+ */
+@Path("/singlesearch")
+@Produces(MediaType.APPLICATION_JSON)
+@RegisterRestClient(baseUri = "https://api.tvmaze.com/")
+public interface TvSeriesProxy {
+     
+	@GET
+	@Path("/shows")
+	TvSerie get(@QueryParam("q") String title);
+	 
+}
